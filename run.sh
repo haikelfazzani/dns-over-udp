c41c8446cddb431a06b413de181acf01e2e4c87d
haikelfazzani <haikel.fazzani@zoho.com>
2024-03-01 22:25:24 +0100

    1.0.0

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ if [ "$(id -u)" -ne "0" ]; then
 fi
 
 # Check and stop service on port 53
-if netstat -tuln | grep ":53 " > /dev/null; then
+if netstat -tuln | grep ":53 " >/dev/null; then
   echo "Stopping service on port 53"
   service=$(netstat -tuln | grep ":53 " | awk '{print $7}')
   pid=$(echo $service | cut -d'/' -f1)
@@ -16,3 +16,15 @@ if netstat -tuln | grep ":53 " > /dev/null; then
 else
   echo "No service found on port 53"
 fi
+
+: <<'END_COMMENT'
+sudo systemctl stop systemd-resolved
+sudo systemctl disable systemd-resolved
+
+sudo systemctl start resolvconf.service
+
+
+sudo ln -sf /run/systemd/resolve/resolv.conf /etc/resolv.conf
+sudo nano /etc/systemd/resolved.conf
+cat /etc/resolv.conf
+END_COMMENT
